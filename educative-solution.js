aa102f68a68957be66bb77bbe265b652db81198b
bdushimi <lespharaons@hotmail.com>
2021-08-19 09:09:07 +0200

    No Solution Right now

--- educative-solution.js
+++ educative-solution.js
@@ -1,64 +1,45 @@
 /*
-Solution #
-Runtime complexity #
-The runtime complexity of this solution is linear, O(n).
+Description #
+Given an array of integers and a value, determine if there are any two integers in the array whose sum is equal to the given value. Return true if the sum exists and return false if it does not.
 
-Memory complexity #
-The memory complexity of this solution is linear, O(n).
+Consider this array and the target sums:
 
-This is the worst case when there are non-overlapping elements in the array.
-
-This problem can be solved in a simple linear scan algorithm. We know that input is sorted by starting timestamps. Here is the approach we are following:
-
-List of input intervals is given, and we’ll keep merged intervals in the output list.
-For each interval in the input list:
-If the input interval is overlapping with the last interval in output list then we’ll merge these two intervals and update the last interval of output list with merged interval.
-Otherwise, we’ll add an input interval to the output list.
-Below is a visual run-through of the above explanation.
-
-The green interval in the input list represents the selected input interval. The last interval in the output list is green, either it is updated by merging this interval and selected input interval or appending input interval.
 */
 
 
+/*
+Runtime complexity #
+The runtime complexity of this solution is linear, O(n)O(n).
+
+Memory complexity #
+The memory complexity of this solution is linear, O(n)O(n).
+
+In this solution, we’ll use the following algorithm to find a pair that add up to the target (say val).
+
+Scan the whole array once and store visited elements in a hash set.
+During scan, for every element e in the array, we check if val - e is present in the hash set i.e. val - e is already visited.
+If val - e is found in the hash set, it means there is a pair (e, val - e) in array whose sum is equal to the given val.
+If we have exhausted all elements in the array and didn’t find any such pair, the function will return false.
+*/
 
 
-class Pair {
-    constructor(first, second) {
-        this.first = first;
-        this.second = second;
-    }
-}
-
-let mergeIntervals = function (v) {
-    if (!v || v.length === 0) {
-        return;
-    }
-
-    let result = [];
-    result.push(new Pair(v[0].first, v[0].second));
-
-    for (let i = 0; i < v.length; i++) {
-        let x1 = v[i].first;
-        let y1 = v[i].second;
-        let x2 = result[result.length - 1].first;
-        let y2 = result[result.length - 1].second;
-
-        if (y2 >= x1) {
-            result[result.length - 1].second = Math.max(y1, y2);
-        } else {
-            result.push(new Pair(x1, y1));
+let findSumOfTwo = function (A, val) {
+    let foundValues = new Set();
+    for (let a in A) {
+        if (foundValues.has(val - A[a])) {
+            return true;
         }
+
+        foundValues.add(A[a]);
     }
-    return result;
+
+    return false;
 };
 
-let v = [new Pair(1, 5), new Pair(3, 7), new Pair(4, 6),
-new Pair(6, 8), new Pair(10, 12), new Pair(11, 15)];
+let v = [5, 7, 1, 2, 8, 4, 3];
+let test = [3, 20, 1, 2, 7];
 
-let result = mergeIntervals(v)
-
-let result_str = ""
-for (let i = 0; i < result.length; i++) {
-    result_str = result_str + "[" + result[i].first + ", " + result[i].second + "] "
+for (i = 0; i < test.length; i++) {
+    let output = findSumOfTwo(v, test[i]);
+    console.log("findSumOfTwo(v, " + test[i] + ") = " + output);
 }
-console.log(result_str)
