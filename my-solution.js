4daedd3b1b6a2b02b66d18e64ad0a3b623d14f62
bdushimi <lespharaons@hotmail.com>
2022-02-16 08:28:53 +0200

    solution not complete

--- my-solution.js
+++
@@ -1,9 +0,0 @@
-const getEqualSubSet = (arraySet) => {
-
-    const sum = arraySet.reduce((acc, current) => acc + current)
-
-    if (sum % 2 !== 0) return false;
-    else return true
-}
-
-console.log(getEqualSubSet([1, 2, 3, 4]));
